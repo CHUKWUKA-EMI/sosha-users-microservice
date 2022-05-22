5c980d484587dc2c7848e9f7bf465d4ca6787aa6
Chukwuka Emi <emichukwuka@gmail.com>
2022-05-22 20:56:51 +0100

    added cert

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,16 +2,23 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { config } from 'dotenv';
+import fs from 'fs';
 
 config();
 
 const ORMCONFIG: TypeOrmModuleOptions = {
-  type: 'postgres',
+  // type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  cert: fs.readFileSync('./root.crt').toString(),
+  ssl: { rejectUnauthorized: false },
+  extra: {
+    options:
+      process.env.NODE_ENV === 'production' ? '--cluster=sosha-users-2132' : '',
+  },
   synchronize: false,
   logging: true,
   migrationsRun: false,
